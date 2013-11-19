cd5731124e81136e0d1545e5f7d6f1f7f06b26e1
Chris Sterling <chrissterlin@gmail.com>
2013-11-18 20:36:57 -0700

    Added beginning of Dij.

--- routed_LS.c
+++ routed_LS.c
@@ -50,9 +50,9 @@ int main(int argc, char *argv[]) {
 	// File format: <source router, source TCP port, destination router, destination TCP port, link cost>
 	//<A,9701,B,9704,4>
 	
-	char source_router = "Z";
+	char source_router = 'Z';
 	int source_tcp_port = -999;
-	char destination_router = "Z";
+	char destination_router = 'Z';
 	int destination_tcp_port = -999;
 	int link_cost = -999;
 	
@@ -69,3 +69,24 @@ int main(int argc, char *argv[]) {
 	
 	return EXIT_SUCCESS;
 }
+
+//Dijkstra’s Shortest Path Algorithm
+//Let N = set of nodes in graph
+//l(i,j) = link cost between i,j (= infinity if not neighbors)
+//SPT = set of nodes in shortest path tree thus far, initially empty except for source
+//S = source node
+//C(n) = cost of path from S to node n
+//Initialize shortest path tree SPT = {S}
+//For each n not in SPT, C(n) = l(s,n)
+//While (SPT<>N)
+	//SPT = SPT U {w} such that C(w) is minimum for all w in (N-SPT). Remember its parent-child link.
+	//For each n in (N-SPT)
+		//C(n) = MIN (C(n), C(w) + l(w,n))
+
+//Initialize shortest path tree SPT = {B}
+//For each n not in SPT, C(n) = l(s,n)
+	//C(E) = 1, C(A) = 3, C(C) = 4, C(others) = infinity
+//Add closest node to the root: SPT = SPT U {E} since C(E) is minimum for all w not in SPT.
+//Recalculate C(n) = MIN (C(n), C(E) + l(E,n)) for all nodes n not yet in SPT
+//Loop again, select node A = has lowest cost path
+//continues on slide 11
